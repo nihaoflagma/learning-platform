a9ad8db5a3c1868ffbede491c7c10bc80133e55e
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:48:11 -0800

    Update Module.java

--- src/main/java/org/example/learningplatform/entity/Module.java
+++ src/main/java/org/example/learningplatform/entity/Module.java
@@ -24,12 +24,12 @@ public class Module {
 
     private int orderIndex;
 
-    // связь с курсом
+    
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
     private Course course;
 
-    // уроки модуля
+    
     @OneToMany(mappedBy = "module", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<Lesson> lessons = new ArrayList<>();
