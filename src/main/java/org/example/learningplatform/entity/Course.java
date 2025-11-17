2f7947a85f122f26073a12deb4c03f47a5a92504
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:47:16 -0800

    Update Course.java

--- src/main/java/org/example/learningplatform/entity/Course.java
+++ src/main/java/org/example/learningplatform/entity/Course.java
@@ -26,24 +26,24 @@ public class Course {
     @Column(length = 2000)
     private String description;
 
-    private int duration; // например, часы
+    private int duration; 
 
     private LocalDate startDate;
 
-    // связь с преподавателем
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "teacher_id", nullable = false)
     private User teacher;
 
-    // модули курса
+    
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<org.example.learningplatform.entity.Module> modules;
 
-    // отзывы студентов
+    
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<CourseReview> reviews;
 
-    // теги (many-to-many)
+   
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "course_tag",
