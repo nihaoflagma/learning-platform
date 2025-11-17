d2233bd6ea09efb7b33ea4c389e78eb36c0a0786
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:50:57 -0800

    Update ModuleRepository.java

--- src/main/java/org/example/learningplatform/repository/ModuleRepository.java
+++ src/main/java/org/example/learningplatform/repository/ModuleRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для работы с модулями курса (Module)
-// CRUD + поиск модулей по курсу можно добавить через @Query
+
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 }
