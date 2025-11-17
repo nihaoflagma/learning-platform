e73da082b72b5b1794fd541d6d3fdc9e08a6bf22
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:50:20 -0800

    Update CourseRepository.java

--- src/main/java/org/example/learningplatform/repository/CourseRepository.java
+++ src/main/java/org/example/learningplatform/repository/CourseRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для работы с сущностью Course (курсы)
-// Позволяет выполнять CRUD: создать, получить, обновить, удалить курс
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
