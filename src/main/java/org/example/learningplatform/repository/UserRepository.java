3295ae5de8b95407c8f808666662cf0b5be576a4
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:52:03 -0800

    Update UserRepository.java

--- src/main/java/org/example/learningplatform/repository/UserRepository.java
+++ src/main/java/org/example/learningplatform/repository/UserRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для работы с пользователями (User)
-// CRUD операции + можно добавить поиск по email, роли и т.д.
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
