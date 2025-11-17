5dc79a97b1ce47bc11ffd677295bc5ade28d45ab
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:47:33 -0800

    Update CourseReview.java

--- src/main/java/org/example/learningplatform/entity/CourseReview.java
+++ src/main/java/org/example/learningplatform/entity/CourseReview.java
@@ -18,25 +18,25 @@ public class CourseReview {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Курс, к которому относится отзыв
+    
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
     private Course course;
 
-    // Студент, который оставил отзыв
+    
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "student_id", nullable = false)
     private User student;
 
     @Column(nullable = false)
-    private int rating; // оценка, например 1-5
+    private int rating; 
 
     @Column(length = 1000)
     private String comment;
 
     private LocalDateTime createdAt;
 
-    // Автоматически проставляем дату при создании
+    
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
