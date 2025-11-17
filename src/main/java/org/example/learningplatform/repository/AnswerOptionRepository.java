00e77cd9db8f33b12d7e91ce78386b3edbc6850f
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:50:08 -0800

    Update AnswerOptionRepository.java

--- src/main/java/org/example/learningplatform/repository/AnswerOptionRepository.java
+++ src/main/java/org/example/learningplatform/repository/AnswerOptionRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.AnswerOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для вариантов ответа (AnswerOption)
-// CRUD операции
+
 @Repository
 public interface AnswerOptionRepository extends JpaRepository<AnswerOption, Long> {
 }
