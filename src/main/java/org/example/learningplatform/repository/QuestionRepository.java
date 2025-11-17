ec1bd502fcf81acb25255c8b8d191b992f4f1db5
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:51:16 -0800

    Update QuestionRepository.java

--- src/main/java/org/example/learningplatform/repository/QuestionRepository.java
+++ src/main/java/org/example/learningplatform/repository/QuestionRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для вопросов теста (Question)
-// CRUD операции
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
