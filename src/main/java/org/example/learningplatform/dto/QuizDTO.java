93bef543cb9158c13b3dac53e1abf49eb71c0451
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:45:57 -0800

    Update QuizDTO.java

--- src/main/java/org/example/learningplatform/dto/QuizDTO.java
+++ src/main/java/org/example/learningplatform/dto/QuizDTO.java
@@ -8,13 +8,13 @@ public class QuizDTO {
     private Long lessonId;
     private List<QuestionDTO> questions;
 
-    // getters and setters
+    
 
     public static class QuestionDTO {
         private String question;
         private List<String> options;
         private int correctOptionIndex;
 
-        // getters and setters
+        
     }
 }
