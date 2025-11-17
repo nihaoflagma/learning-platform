d8224ae9f2cdf4c5f7e439e5d23856aeedde6dac
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:51:06 -0800

    Update ProfileRepository.java

--- src/main/java/org/example/learningplatform/repository/ProfileRepository.java
+++ src/main/java/org/example/learningplatform/repository/ProfileRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для профиля пользователя (Profile)
-// CRUD операции
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
