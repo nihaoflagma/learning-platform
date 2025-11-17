056edcf4c040fc40df8a42a1357370333a6bece3
nihaoflagma <114833329+nihaoflagma@users.noreply.github.com>
2025-11-17 12:51:52 -0800

    Update TagRepository.java

--- src/main/java/org/example/learningplatform/repository/TagRepository.java
+++ src/main/java/org/example/learningplatform/repository/TagRepository.java
@@ -4,8 +4,7 @@ import org.example.learningplatform.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// Репозиторий для тегов (Tag), которые прикрепляются к курсам
-// CRUD операции
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
